f0ac2fc9b4133c024a0782bf854f2f8a9cf3ed5c
fjmonge00 <fjmonge00@gmail.com>
2021-05-07 23:17:42 +0200

    OA add drupal

--- COMANDOS/oa.sh
+++ COMANDOS/oa.sh
@@ -10,8 +10,8 @@ export OAK8SLOG="/var/log/hosting"
 # Consulta en MariaDB
 echo "SELECT id,app,cliente FROM servicios WHERE fecha > NOW() - INTERVAL 3 MINUTE" | mariadb -N -B hosting > $LANZADERA/cola/servicios.csv
 # Prepara CSV
-sed -i 's/\t/,/g' $LANZADERA/cola/servicios.csv
-if [ ! -s $LANZADERA/cola/servicios.csv ] 
+sed -i 's/\t/,/g' $LANZADERA/cola/servicios.csv # Control errores Eliminar Espacios
+if [ ! -s $LANZADERA/cola/servicios.csv ] # Si no tiene datos (Vacio)
     then
         echo "Ningun servicio en cola: $(date +"%d-%m-%Y-%R:%S")" > $LANZADERA/cola/servicios.csv
         echo "Proximo lanzamiento: $(date --date="+3 min" +"%d-%m-%Y-%R:%S")" >> $LANZADERA/cola/servicios.csv
@@ -22,7 +22,7 @@ if [ ! -s $LANZADERA/cola/servicios.csv ]
         echo "" >> $OAK8SLOG/oa/serviciosLanzados.log
         while IFS=, read id app cliente
         do
-            mkdir $LANZADERA/${app}-${id} # Carpeta base lanzadera del servicio
+            mkdir $LANZADERA/${app}-${id} # Carpeta base de lanzadera del servicio
             case ${app} in
                 'joomla') sh OAJoomla.sh $id $cliente
                 ;;
@@ -32,7 +32,7 @@ if [ ! -s $LANZADERA/cola/servicios.csv ]
                 ;;
                 'mediawiki') echo "mediawiki"
                 ;;
-                'drupal') echo "drupal"
+                'drupal') sh OADrupal.sh $id $cliente
                 ;;
                 *) echo "Error de lanzamiento: $(date +"%d-%m-%Y-%R:%S")" >> $OAK8SLOG/oa/serviciosLanzados.log
                    echo "Valor de Id: ${id}" >> $OAK8SLOG/oa/serviciosLanzados.log
