176c82556ca64d638f4c53cdb3e004f1d3ff947b
fjmonge00 <fjmonge00@gmail.com>
2021-05-28 00:00:03 +0200

    Cambios

--- COMANDOS/VPS/ParaLaOA.sh
+++ COMANDOS/VPS/ParaLaOA.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-id=9500
+id=9512
 cliente=inerco
 so=Debian10
 vCPU=2
@@ -7,14 +7,15 @@ vRAM=4
 DiscoAnadido=10
 Plan="Instalar LAMP"
 contrasena=$(cat /dev/urandom | tr -dc [:upper:][:lower:][:digit:] | head -c32)
-contrasena2=$(cat /dev/urandom | tr -dc [:upper:][:lower:][:digit:] | head -c32)
-echo "$contrasena"
-echo "$contrasena2"
+# contrasena=Coria21
+echo "$cliente-$id" >> datos.txt
+echo "$contrasena" >> datos.txt
 # Preparacion
 # Creación o Clonado
 virt-clone --original Base$so --name $cliente-$id --file $VPS/$cliente-$id-$so.qcow2 #Contra Barra evita anadir Base a la variable
 # Preparacion Maquina
-virt-sysprep -d $cliente-$id --root-password password:$contrasena --hostname $cliente-$id
+# virt-sysprep -d $cliente-$id --root-password password:$contrasena --hostname $cliente-$id
+virt-sysprep -d $cliente-$id --root-password password:$contrasena --hostname $cliente-$id --run preparacion.sh
 # Configuración de Hardware
 if [ $vCPU -gt 1 ]
     then
@@ -28,5 +29,21 @@ if [ $DiscoAnadido -gt 0 ]
     then
         qemu-img resize $VPS/$cliente-$id-$so.qcow2 +$DiscoAnadido\G
 fi
-virsh start --domain $cliente-$id
+virsh start --domain $cliente-$id # Arranque
+# sshpass -f password.txt ssh-copy-id root@192.168.122.157 -p 3022
+esperar=1
+while [ $esperar -eq 1 ] 
+do
+    $conexion=$(ping -c1 $ip | grep "1 packets transmitted, 1 received, 0% packet loss" |wc -l)
+    case $conexion in
+        1) sshpass -f $contrasena ssh-copy-id root@$ip -p 3022
+           esperar=0
+        ;;
+        *) sleep 2
+           esperar=1
+        ;;
+    esac
+done
 echo "ANSIBLE: $Plan"
+arp -n | grep $(virsh dumpxml $cliente-$id | grep mac\ address | cut -d \' -f 2 ) | cut -d \  -f 1 >> datos.txt
+echo ""  >> datos.txt
